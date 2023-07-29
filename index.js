1861844e847d471484afb6aa427bc32bbae111ad
Luis Reyes <lualreye@gmail.com>
2023-07-28 22:24:44 -0500

    add: cors on project

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const dbConnect = require('./config/mongo');
 
 dotenv.config();
 
+const whiteList = [
+  'http://localhost:5173',
+  // TODO: add app app domain
+  // TODO: add app sandbox domain
+];
+
 const app = express();
+
+app.use(cors({ origin: whiteList }));
+
+app.use(express.json());
+
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
