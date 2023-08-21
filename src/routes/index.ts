5df2d058d553ad8d041f0017ec320ebbcf6afe32
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-08-21 14:30:15 -0500

    fix: reading routes properly, get working as expected

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,10 @@
-import express from "express";
-import fs from "fs";
-import path from "path";
+import express, { Application } from 'express';
+import Plans from './plans';
 
-const router = express.Router();
+function routerApi(app: Application) {
+  const router = express.Router();
+  app.use('/api', router);
+  router.use(Plans);
+}
 
-// Lee todos los archivos en el directorio actual
-fs.readdirSync(__dirname)
-  .filter((file) => file.endsWith('.ts') && file !== 'index.ts')  // Filtra solo los archivos .ts (excluyendo index.ts)
-  .forEach((file) => {
-    const route = require(path.join(__dirname, file));  // Importa el archivo de ruta
-    router.use(`/${file.slice(0, -3)}`, route);  // Usa las rutas en la aplicación Express (elimina la extensión .ts del nombre del archivo para el nombre de la ruta)
-  });
-
-export default router;
+export default routerApi;
