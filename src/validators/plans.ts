1b05a4eca16366e7e38a15156f484fc40bc7a92e
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-08-23 11:45:53 -0500

    fix: validator, update: validator

--- src/validators/plans.ts
+++ src/validators/plans.ts
@@ -1,30 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
 import { check } from 'express-validator';
-import { body } from 'express-validator';
 
 import validateResults from '../utils/handleValidator';
 
-export const planValidator = [
-  body('name')
+const planValidatorCreate = [
+  check('name')
     .notEmpty()
     .isString()
     .withMessage('Name is required')
     .isLength({ max: 20 })
     .withMessage('Max Length 20 characters'),
 
-  body('description')
+  check('description')
     .notEmpty()
     .withMessage('Description is required')
     .isLength({ max: 500 })
     .withMessage('Max Length 500 characters'),
 
-  body('price')
+  check('price')
     .notEmpty()
     .withMessage('Price is required')
     .isNumeric()
     .withMessage('Only number is allowed'),
 
-  body('image')
+  check('image')
     .notEmpty()
     .withMessage('Image is required')
     .isURL()
@@ -34,3 +33,58 @@ export const planValidator = [
     return validateResults(req, res, next)
   }
 ];
+
+
+const planValidatorUpdate = [
+  check('id')
+    .exists()
+    .notEmpty()
+    .isMongoId(),
+
+  check('name')
+    .notEmpty()
+    .isString()
+    .withMessage('Name is required')
+    .isLength({ max: 20 })
+    .withMessage('Max Length 20 characters'),
+
+  check('description')
+    .notEmpty()
+    .withMessage('Description is required')
+    .isLength({ max: 500 })
+    .withMessage('Max Length 500 characters'),
+
+  check('price')
+    .notEmpty()
+    .withMessage('Price is required')
+    .isNumeric()
+    .withMessage('Only number is allowed'),
+
+  check('image')
+    .notEmpty()
+    .withMessage('Image is required')
+    .isURL()
+    .withMessage('No image URL'),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    return validateResults(req, res, next)
+  }
+];
+
+const planValidatorDelete = [
+  check('id')
+    .exists()
+    .notEmpty()
+    .isMongoId(),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    return validateResults(req, res, next)
+  }
+];
+
+
+export {
+  planValidatorCreate,
+  planValidatorUpdate,
+  planValidatorDelete
+}
