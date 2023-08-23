46ec2c26d98bf46abc27636ec6c9d24b686a9fa5
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-08-23 12:01:23 -0500

    rm: unnecessary comments

--- src/routes/plans.ts
+++ src/routes/plans.ts
@@ -5,16 +5,12 @@ import { getPlans, createPlan, updatePlan, deletePlan } from '../controllers/pla
 
 const router = express.Router();
 
-// GET: Recibe todo lo que contiene el plan
 router.get('/plans', getPlans);
 
-// POST: Postea el nuevo plan
 router.post('/plan', planValidatorCreate,createPlan);
 
-// PUT: Actualiza un plan creado
 router.put('/plan/:id', planValidatorUpdate, updatePlan);
 
-// DELETE: Deletea un plan existente
 router.delete('/plan/:id', planValidatorDelete, deletePlan);
 
 export default router;
