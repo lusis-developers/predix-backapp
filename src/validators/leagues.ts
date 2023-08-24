19b413e398b257ba7e939d3883c92b35992f1ce0
robmoncayonabux <roberto.moncayo@nabux.ec>
2023-08-23 21:20:05 -0500

    feature: validator for leagues

---
+++ src/validators/leagues.ts
@@ -0,0 +1,59 @@
+import { NextFunction, Request, Response } from 'express';
+import { check } from 'express-validator';
+
+import validateResults from '../utils/handleValidator';
+
+const leagueValidatorCreate = [
+  check('name')
+    .notEmpty()
+    .isString()
+    .withMessage('Name is required')
+    .isLength({ max: 20 })
+    .withMessage('Max Length 20 characters'),
+
+  check('image')
+    .notEmpty()
+    .withMessage('Image is required')
+    .isURL()
+    .withMessage('No image URL'),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    return validateResults(req, res, next)
+  }
+];
+const leagueValidatorUpdate = [
+  check('name')
+    .notEmpty()
+    .isString()
+    .withMessage('Name is required')
+    .isLength({ max: 20 })
+    .withMessage('Max Length 20 characters'),
+
+  check('image')
+    .notEmpty()
+    .withMessage('Image is required')
+    .isURL()
+    .withMessage('No image URL'),
+    
+  (req: Request, res: Response, next: NextFunction) => {
+    return validateResults(req, res, next)
+  }
+];
+
+const leagueValidatorDelete = [
+  check('id')
+    .exists()
+    .notEmpty()
+    .isMongoId(),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    return validateResults(req, res, next)
+  }
+];
+
+
+export {
+  leagueValidatorCreate,
+  leagueValidatorUpdate,
+  leagueValidatorDelete
+}
