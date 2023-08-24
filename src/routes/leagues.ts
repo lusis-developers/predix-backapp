7201f8bb989831112bc38a6903025ec36f0d1cc2
robmoncayonabux <roberto.moncayo@nabux.ec>
2023-08-23 21:21:03 -0500

    feature: validator for leagues

--- src/routes/leagues.ts
+++ src/routes/leagues.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getLeagues, createLeague, updateLeague, deleteLeague } from '../controllers/leagues';
+import { leagueValidatorCreate, leagueValidatorDelete, leagueValidatorUpdate } from '../validators/leagues';
 
 const router = express.Router();
 
@@ -7,12 +8,12 @@ const router = express.Router();
 router.get('/leagues', getLeagues);
 
 // POST: Postea el nuevo league
-router.post('/league', createLeague);
+router.post('/league', leagueValidatorCreate, createLeague);
 
 // PUT: Actualiza un league creado
-router.put('/league/:id', updateLeague);
+router.put('/league/:id', leagueValidatorUpdate, updateLeague);
 
 // DELETE: Deletea un league existente
-router.delete('/league/:id', deleteLeague);
+router.delete('/league/:id', leagueValidatorDelete, deleteLeague);
 
 export default router;
