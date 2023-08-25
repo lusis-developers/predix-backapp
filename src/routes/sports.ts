38a10e2e3d2a71f5b6cd59fd0f430b77a8353c96
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-08-24 19:02:50 -0500

    feat: add sport images upoloaded

--- src/routes/sports.ts
+++ src/routes/sports.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+
+import uploadMiddleware from '../middlewares/handleImage';
 import {
   getSports,
   createSport,
   updateSport,
-  deleteSport
+  deleteSport,
+  uploadSportImage
 } from '../controllers/sports';
 import {
   sportValidatorCreate,
@@ -16,10 +19,17 @@ const router = express.Router();
 // GET: Recibe todo lo que contiene el Sport
 router.get('/sports', getSports);
 
+// TODO: endpoint to upload image to GCP before createPlan on POST METHOD
+router.post(
+  '/sportImage',
+  uploadMiddleware.single('sportImage'),
+  uploadSportImage
+);
+
 // POST: Postea el nuevo Sport
 router.post('/sport', sportValidatorCreate, createSport);
 
-// PUT: Actualiza un bet creado
+// PUT: Actualiza un sport creado
 router.put('/sport/:id', sportValidatorUpdate, updateSport);
 
 // DELETE: Deletea un bet existente
