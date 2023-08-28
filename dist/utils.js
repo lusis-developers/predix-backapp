e072af0d46cbc413efba1ddcf6966fdffc7ae3fb
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-08-28 16:28:46 -0500

    fix: deployment settings

--- dist/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getting = void 0;
-const getting = (req, res) => {
-    console.log(res, res);
-};
-exports.getting = getting;
