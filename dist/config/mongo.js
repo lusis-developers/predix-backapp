cb1b473a2022d9bf9e9e92f85a1dbbe98f281f78
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-08-28 16:41:38 -0500

    fix: dist forder added to compilation

---
+++ dist/config/mongo.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+async function dbConnect() {
+    try {
+        const DB_URI = process.env.DB_URI;
+        if (!DB_URI) {
+            throw new Error('No mongodb URI');
+        }
+        await mongoose_1.default.connect(DB_URI);
+        console.log('*** CONEXION CORRECTA ***');
+    }
+    catch (error) {
+        console.log('*** ERROR DE CONEXION ***');
+        console.log('error', error);
+    }
+}
+exports.default = dbConnect;
---
+++ dist/enum/imagesEnum.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ImagesEnum = void 0;
+var ImagesEnum;
+(function (ImagesEnum) {
+    ImagesEnum["SPORT"] = "sport";
+    ImagesEnum["PLAN"] = "plan";
+    ImagesEnum["LEAGUE"] = "league";
+})(ImagesEnum || (exports.ImagesEnum = ImagesEnum = {}));
