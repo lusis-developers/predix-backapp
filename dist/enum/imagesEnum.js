1c0efda7175f7e79091108b203943cd73659f83f
lualreye <66281579+lualreye@users.noreply.github.com>
2023-08-28 21:28:17 +0000

    Delete dist directory

--- dist/enum/imagesEnum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ImagesEnum = void 0;
-var ImagesEnum;
-(function (ImagesEnum) {
-    ImagesEnum["SPORT"] = "sport";
-    ImagesEnum["PLAN"] = "plan";
-    ImagesEnum["LEAGUE"] = "league";
-})(ImagesEnum || (exports.ImagesEnum = ImagesEnum = {}));
