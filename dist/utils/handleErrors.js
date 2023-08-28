19cccd77859109f7361948cc8f984320ed1a73a1
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-08-28 16:46:21 -0500

    rm: dist directory

--- dist/utils/handleErrors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function handleHttpError(res, message = 'Oops, somethins happened', code = 403) {
-    res.status(code);
-    res.send({ error: message });
-}
-exports.default = handleHttpError;
