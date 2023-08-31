175c2ffdf3f5ac27e4190a9ab79aa4c58d5a8d9d
robmoncayonabux <roberto.moncayo@nabux.ec>
2023-08-31 18:25:58 -0500

    feature: users controller

---
+++ src/controllers/users.ts
@@ -0,0 +1,90 @@
+import { Request, Response } from 'express';
+import { matchedData } from 'express-validator';
+
+import gcpImageUpload from '../services/gcpImageUpload';
+import handleHttpError from '../utils/handleErrors';
+import { ImagesEnum } from '../enum/imagesEnum';
+import models from '../models/index';
+
+/**
+ * Get a bet item from the database
+ * @param req
+ * @param res
+ */
+async function getUser(req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const data = await models.users.findById({ _id: id });
+    res.send({ data });
+  } catch (error) {
+    handleHttpError(res, 'Cannot get user');
+  }
+}
+
+/**
+ * Upload a user image to the database
+ * @param req
+ * @param res
+ */
+async function uploadUserImage(req: Request, res: Response) {
+  try {
+    const { file } = req;
+    const result = await gcpImageUpload(file!, ImagesEnum.USER);
+    const fileData = {
+      url: result,
+      filename: result.split('/')[2]
+    };
+    const data = await models.userImages.create(fileData);
+    res.send({ data });
+  } catch (error) {
+    handleHttpError(res, 'Error uploading file');
+  }
+}
+
+/**
+ * Create a user item to the database
+ * @param req
+ * @param res
+ */
+async function createUser(req: Request, res: Response) {
+  const { body } = req;
+  try {
+    const newuser = await models.users.create(body);
+    res.send(newuser);
+  } catch (error) {
+    handleHttpError(res, 'Cannot create users');
+  }
+}
+
+/**
+ * Update a user item from the database
+ * @param req
+ * @param res
+ */
+async function updateUser(req: Request, res: Response) {
+  try {
+    const { id, ...body } = matchedData(req);
+    await models.users.findByIdAndUpdate(id, body);
+    res.send({
+      message: 'User updated'
+    });
+  } catch (error) {
+    handleHttpError(res, 'Cannot update user');
+  }
+}
+
+/**
+ * Delete an user item from the database
+ * @param req
+ * @param res
+ */
+async function deleteUser(req: Request, res: Response) {
+  try {
+    await models.users.findOneAndDelete({ _id: req.params.id });
+    res.send({ message: 'User deleted successfully' });
+  } catch (error) {
+    handleHttpError(res, 'Cannot delete user');
+  }
+}
+
+export { getUser, createUser, updateUser, deleteUser, uploadUserImage };
