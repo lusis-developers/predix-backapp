7e08fbaf54248abce3ccc860ccce0c85888265ea
robmoncayonabux <roberto.moncayo@nabux.ec>
2023-08-31 21:15:24 -0500

    add: route to get all users

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -4,9 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const handleImage_1 = __importDefault(require("../middlewares/handleImage"));
 const users_1 = require("../validators/users");
 const users_2 = require("../controllers/users");
 const router = express_1.default.Router();
+router.get('/users', users_2.getUsers);
+// TODO: endpoint to upload image to GCP before createPlan on POST METHOD
+router.post('/UserImage', handleImage_1.default.single('userImage'), users_2.uploadUserImage);
 // TODO: get specific user
 router.get('/users/:id', users_2.getUser);
 router.post('/users', users_1.userValidatorCreate, users_2.createUser);
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,18 +1,31 @@
 import express from 'express';
 
+import uploadMiddleware from '../middlewares/handleImage';
+
 import {
   userValidatorCreate,
   userValidatorDelete,
   userValidatorUpdate
 } from '../validators/users';
 import {
+  getUsers,
   getUser,
   updateUser,
   deleteUser,
-  createUser
+  createUser,
+  uploadUserImage
 } from '../controllers/users';
 
 const router = express.Router();
+
+router.get('/users', getUsers);
+
+// TODO: endpoint to upload image to GCP before createPlan on POST METHOD
+router.post(
+  '/UserImage',
+  uploadMiddleware.single('userImage'),
+  uploadUserImage
+);
 
 // TODO: get specific user
 router.get('/users/:id', getUser);
