2cc56e928b2ed03eff583a4fac29c4143d6a32e5
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-09-04 17:37:52 -0500

    FIX: line break

--- src/validators/users.ts
+++ src/validators/users.ts
@@ -10,38 +10,45 @@ const userValidatorCreate = [
     .withMessage('Name is required')
     .isLength({ max: 20 })
     .withMessage('Max Length 20 characters'),
+
   check('userimage')
     .notEmpty()
     .withMessage('Image is required')
     .isURL()
     .withMessage('Invalid image URL'),
+
   check('mail')
     .notEmpty()
     .withMessage('Mail is required')
     .isEmail()
     .withMessage('Invalid email format'),
+
   check('phone')
     .notEmpty()
     .withMessage('Phone is required')
     .isString()
     .withMessage('Only number is allowed'),
+
   check('birthdate')
     .exists()
     .notEmpty()
     .isISO8601()
     .withMessage('Date is required'),
+
   check('twitter')
     .optional()
     .isString()
     .withMessage('Invalid twitter handle')
     .isLength({ max: 20 })
     .withMessage('Max Length 20 characters'),
+
   check('instagram')
     .optional()
     .isString()
     .withMessage('Invalid instagram handle')
     .isLength({ max: 20 })
     .withMessage('Max Length 20 characters'),
+
   (req: Request, res: Response, next: NextFunction) => {
     return validateResults(req, res, next);
   }
@@ -54,26 +61,33 @@ const userValidatorUpdate = [
     .withMessage('Name must be a string')
     .isLength({ max: 20 })
     .withMessage('Max Length 20 characters'),
+
   check('userimage').optional().isURL().withMessage('Invalid image URL'),
+
   check('mail').optional().isEmail().withMessage('Invalid email format'),
+
   check('phone').optional().isNumeric().withMessage('Phone must be a number'),
+
   check('birthdate')
     .exists()
     .optional()
     .isISO8601()
     .withMessage('Invalid date format'),
+
   check('twitter')
     .optional()
     .isString()
     .withMessage('Invalid twitter handle')
     .isLength({ max: 20 })
     .withMessage('Max Length 20 characters'),
+
   check('instagram')
     .optional()
     .isString()
     .withMessage('Invalid instagram handle')
     .isLength({ max: 20 })
     .withMessage('Max Length 20 characters'),
+
   (req: Request, res: Response, next: NextFunction) => {
     return validateResults(req, res, next);
   }
