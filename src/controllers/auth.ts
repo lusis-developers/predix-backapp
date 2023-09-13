9af0f5de669c4a379131bdde10bda58acf0e09e7
robmoncayonabux <roberto.moncayo@nabux.ec>
2023-09-13 18:39:08 -0500

    feature: auth controller working

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,11 +2,22 @@ import { Request, Response } from 'express';
 
 import handleHttpError from '../utils/handleErrors';
 import models from '../models/index';
+import { encrypt } from '../middlewares/handleJwt';
+import tokenSign from '../utils/handleJwt';
+
 async function createAuthRegister(req: Request, res: Response) {
-  const { body } = req;
   try {
-    const newauth = await models.auth.create(body);
-    res.send(newauth);
+    const { body } = req;
+    const encryptedPassword = await encrypt(req.body.password);
+    const newBody = { ...body, password: encryptedPassword };
+    const newAuth = await models.auth.create(newBody);
+    newAuth.set('password', undefined, { strict: false });
+
+    const data = {
+      token: await tokenSign(newAuth as any),
+      user: newAuth
+    };
+    res.send({ data });
   } catch (error) {
     handleHttpError(res, 'Cannot create auth');
   }
