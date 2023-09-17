af18dc16bbbafd58c6b2a6123a0a246688238f31
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-09-17 14:28:17 -0500

    ADD: environment variables for dev

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
 
+import { Environment_Variables } from '../enum/EnvironmentVariables';
+
 async function dbConnect(): Promise<void> {
   try {
     let DB_URI = process.env.DB_URI;
 
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === Environment_Variables.DEVELOPMENT) {
+      DB_URI = process.env.DB_URI_DEVELOPMENT;
+    }
+
+    if (process.env.NODE_ENV === Environment_Variables.PRODUCTION) {
       DB_URI = process.env.DB_URI_PRODUCTION;
     }
 
